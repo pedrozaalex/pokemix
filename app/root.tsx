5637b1acb4d9c0a49ab482735342bacf8883272c
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2022-02-01 22:06:48 -0300

    🎉 Beginning project!

---
+++ app/root.tsx
@@ -0,0 +1,30 @@
+import type { MetaFunction } from 'remix';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix';
+
+export const meta: MetaFunction = () => ({ title: 'New Remix App' });
+
+export default function App(): JSX.Element {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+      </body>
+    </html>
+  );
+}
